3c02f6ebc44bd3b61e47b708ef8039efa4be7e5a
zwiebus <zwiebus@users.noreply.github.com>
2022-12-20 14:59:38 +0100

    Update to compact version
    This branch is designed as a compact standalone version with its own window.

---
+++ js/windowsOpen.js
@@ -0,0 +1,5 @@
+function playerOpen(sta){
+    var x = screen.width/2 - 350/2;
+    var y = screen.height/2 - 640/2;
+    window.open(sta.href, 'sharegplus','height=640,width=350,left='+x+',top='+y);
+}
